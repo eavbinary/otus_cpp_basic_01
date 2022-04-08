152ed5adfc43392110ebb700ec210335da364bc0
Alexandr Emel'yanov <eav.binary@ya.ru>
2022-04-08 07:19:45 +0300

    002

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,11 @@
 #include "example.h"
-#include "move_me_out.h"
+#include "lib/move_me_out.h"
 #include <iostream>
 
 int main() {
 	std::cout << "Hello, World!" << std::endl;
-
 	example_function();
 	todo_move_me_out();
-	
+	std::cout << "New line by CLin" << std::endl;
 	return 0;
 }
